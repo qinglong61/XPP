e05055118d307e83261563bf7fe964b5eb71406b
duanyu <qinglong61@163.com>
2016-01-18 18:05:31 +0800

    complete basic http proxy

--- Toufu.js
+++ Toufu.js
@@ -1,69 +1,33 @@
 const http = require('http');
-const https = require('https');
 const url = require('url');
 
-const hostname = '127.0.0.1';
-const port = 8989;// port 3000 for koding.com
+const port = 8989;
 
-https.Server({
-    //  key: fs.readFileSync('/path/to/server.key'),
-    //  cert: fs.readFileSync('/path/to/server.crt')
-}, function(req, res){
-    res.writeHead(200);
-    res.end("hello world\n");
-}).listen(443, function(err){
-     console.log("https listening on port: 443");
-});
+http.createServer((request, response) => {//request is an instance of http.IncomingMessage and response is an instance of http.ServerResponse.
+    var requestURL = request.url;
+    console.log(requestURL);
+    var hostname = url.parse(requestURL, true).hostname;
+    var path = url.parse(requestURL, true).path;
+    var options = {
+        hostname: hostname,
+        method: request.method,
+        path: path,
+        headers: request.headers
+    };
+    var req = http.request(options, function(res) {
+        response.writeHead(res.statusCode, res.headers);
+        res.on('data', function (chunk) {
+            response.write(chunk);
+        }).on('end', function() {
+            response.end();
+        });
+    });
 
-http.createServer((req, res) => {
-    var requestURL = req.url;
-    console.log(req);
-    if (requestURL) {
-        var hostname = url.parse(requestURL, true).hostname;
-        var path = url.parse(requestURL, true).path;
-        var headers = req.headers;
-        headers.host = hostname;
-        var options = {
-            hostname: hostname,
-            path: path,
-            headers: headers
-        };
-        var protocol = url.parse(requestURL).protocol;
-        console.log(protocol);
-        if (protocol == 'http:') {
-            http.get(options, function(res1) {
-                res.writeHead(res1.statusCode, res1.headers);
-                // res1.setEncoding('utf8');
-                res1.on('data', function (chunk) {
-                    res.write(chunk);
-                });
-                res1.on('end', function() {
-                    res.end();
-                });
-            }).on('error', function(e) {
-                console.error(e);
-            });
-        } else if (protocol == 'https:') {
-            console.log(requestURL);
-            https.get(options, function(res2) {
-                res.writeHead(res2.statusCode, res2.headers);
-                res2.on('data', function(chunk) {
-                    res.write(chunk);
-                });
-                res2.on('end', function() {
-                    res.end();
-                });
-            }).on('error', function(e) {
-                console.error(e);
-            });
-        } else {
-            res.writeHead(200);
-            res.end("Server is running\n");
-        }
-    } else {
-        res.writeHead(200);
-        res.end("Server is running\n");
-    }
-}).listen(port, hostname, () => {
-    console.log(`Server running at http:\/\/${hostname}:${port}\/`);
+    request.on('data', function (postChunk) {
+        req.write(postChunk)
+    }).on('end', function() {
+        req.end();
+    });
+}).listen(port, () => {
+    console.log(`HTTP Server running at port:${port}`);
 });
